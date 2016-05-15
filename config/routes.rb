cb712e228c7514ab859ebcea042e3c90be7f5bd7
sacha3768 <petgen722@gmail.com>
2016-05-15 18:38:19 +0200

    Removing the display of errors on the inscription formular for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :users
   root 'home#index'
+  get  'inscription'  => 'users#new'
+  post 'inscription'  => 'users#create', as: 'users'
+  get  'membres'      => 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
